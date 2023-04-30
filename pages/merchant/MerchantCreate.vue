92de0cef589eeb71fdbbe6caeb8428d8d2977dff
FredNg <wjhfred@outlook.com>
2023-04-30 16:46:50 +0800

    fix: 修复图片上传的bug

--- pages/merchant/MerchantCreate.vue
+++ pages/merchant/MerchantCreate.vue
@@ -339,7 +339,7 @@
 				})
 				OBSUpload(event.file.url, (res) => {
 					if (res.statusCode == '204') {
-						const spliter = res.header.Location.split('/');
+						const spliter = res.header.Location[0].split('/');
 						const fileName = spliter[spliter.length - 1];
 						console.log(fileName)
 						const url = config.EndPoint + '/' + fileName;
