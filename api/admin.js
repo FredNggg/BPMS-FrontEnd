14d964eb84a29fbe030fc2211d60cefed591a488
FredNg <wjhfred@outlook.com>
2023-05-04 15:51:31 +0800

    update 接口

--- api/admin.js
+++ api/admin.js
@@ -2,25 +2,25 @@ import Request from '@/utils/request.js';
 import operate from '@/common/operate.js';
 
 let request = (new Request()).http;
-const PREFIX = '/admin'
+const PREFIX = '/marketer'
 
 export const getAllMarketers = function(currPage, pageSize = 10){
 	return request({
-		url: `${PREFIX}/marketer/all?currPage=${currPage}&pageSize=${pageSize}`,
+		url: `${PREFIX}/all?currPage=${currPage}&pageSize=${pageSize}`,
 		method: 'GET',
 	})
 }
 
 export const getMarketersByState = function(state, currPage, pageSize = 10){
 	return request({
-		url: `${PREFIX}/marketer/${state}?currPage=${currPage}&pageSize=${pageSize}`,
+		url: `${PREFIX}/${state}?currPage=${currPage}&pageSize=${pageSize}`,
 		method: 'GET',
 	})
 }
 
 export const modifyState = function(id, state){
 	return request({
-		url: `${PREFIX}/marketer/state/${id}?state=${state}`,
+		url: `${PREFIX}/state/${id}?state=${state}`,
 		method: 'POST',
 	})
 }
--- api/user.js
+++ api/user.js
@@ -2,7 +2,8 @@ import Request from '@/utils/request.js';
 import operate from '@/common/operate.js';
 
 let request = (new Request()).http;
-const PREFIX = '/user'
+const PREFIX_USER = '/user'
+const PREFIX_MARKETER = '/marketer'
 
 export const register = function(name, phone, institutionId, position, state) {
 	const data = {
@@ -13,7 +14,7 @@ export const register = function(name, phone, institutionId, position, state) {
 		state: state
 	};
 	return request({
-		url: `${PREFIX}/register/marketer`,
+		url: `${PREFIX_MARKETER}/register`,
 		method: 'POST',
 		data: data,
 	})
@@ -21,35 +22,36 @@ export const register = function(name, phone, institutionId, position, state) {
 
 export const sendVerificationCode = function(phone){
 	return request({
-		url: `${PREFIX}/verificationCode/send/?phone=${phone}`,
+		url: `${PREFIX_USER}/verificationCode/send/?phone=${phone}`,
 		method: 'GET',
 	})
 }
 
 export const checkVerificationCode = function(phone, code){
 	return request({
-		url: `${PREFIX}/verificationCode/check/?phone=${phone}&code=${code}`,
+		url: `${PREFIX_USER}/verificationCode/check/?phone=${phone}&code=${code}`,
 		method: 'GET',
 	})
 }
 
 export const phoneLogin = function(userRole, phone, code){
 	return request({
-		url: `${PREFIX}/login/phone?userRole=${userRole}&phone=${phone}&code=${code}`,
+		url: `${PREFIX_USER}/login/phone?userRole=${userRole}&phone=${phone}&code=${code}`,
 		method: 'POST',
 	})
 }
 
 export const wechatLogin = function(userRole, weChatLoginCode){
 	return request({
-		url: `${PREFIX}/login/wechat?userRole=${userRole}&weChatLoginCode=${weChatLoginCode}`,
+		url: `${PREFIX_USER}/login/wechat?userRole=${userRole}&weChatLoginCode=${weChatLoginCode}`,
 		method: 'POST',
 	})
 }
 
 export const wechatBind = function(userRole, phone, weChatLoginCode){
 	return request({
-		url: `${PREFIX}/bind/wechat?userRole=${userRole}&phone=${phone}&weChatLoginCode=${weChatLoginCode}`,
+		url: `${PREFIX_USER
+		}/bind/wechat?userRole=${userRole}&phone=${phone}&weChatLoginCode=${weChatLoginCode}`,
 		method: 'POST',
 	})
 }
